3c77b0c8c0bef9a75332f6e1a6d191d20349c3e3
quentinfu <fqiantong@gmail.com>
2017-11-29 01:25:42 -0800

    Add files via upload

--- renderer.js
+++ renderer.js
@@ -109,3 +109,128 @@ function revertSettings() {
   flashFlag = false;
   upload();
 }
+
+
+
+
+
+let flags = 0;
+let success = false;
+
+  const dot1 = document.getElementById("one");
+  dot1.addEventListener("mouseenter", mouseEnter1);
+  dot1.addEventListener("mouseleave", mouseLeave1);
+  console.log("enter");
+
+
+function mouseEnter1() {
+    
+    if (!success) {
+      if (flags === 0) {
+        document.getElementById("two").style.fill = "red";
+        document.getElementById("instruction").innerHTML = "Now move your mouse to the top right dot"
+        flags++
+      } else if (flags === 4) {
+        document.getElementById("instruction").innerHTML = "Success!"
+        success = true
+
+      } else {
+        document.getElementById("instruction").innerHTML = "Failed. Move your mouse to the top left dot"
+        clear();
+        flags = 0
+      }
+    }
+}
+
+function mouseLeave1() {
+    //document.getElementById("two").style.fill = "blue";
+}
+
+ const dot2 = document.getElementById("two");
+  dot2.addEventListener("mouseenter", mouseEnter2);
+  dot2.addEventListener("mouseleave", mouseLeave2);
+  console.log("enter");
+
+
+function mouseEnter2() {
+ 
+    
+     if (!success) {
+      if (flags === 1) {
+      document.getElementById("three").style.fill = "red";
+       document.getElementById("two").style.fill = "blue";
+      
+        document.getElementById("instruction").innerHTML = "Now move your mouse to the bottom right dot"
+        flags++
+      } else {
+        document.getElementById("instruction").innerHTML = "Failed. Move your mouse to the top left dot"
+        clear();
+        flags = 0
+      }
+    }
+}
+
+function mouseLeave2() {
+    //document.getElementById("three").style.fill = "blue";
+}
+ const dot3 = document.getElementById("three");
+  dot3.addEventListener("mouseenter", mouseEnter3);
+  dot3.addEventListener("mouseleave", mouseLeave3);
+  console.log("enter");
+
+
+function mouseEnter3() {
+  
+   
+     if (!success) {
+            if (flags === 2) {
+
+       document.getElementById("four").style.fill = "red";
+      document.getElementById("three").style.fill = "blue";
+        document.getElementById("instruction").innerHTML = "Now move your mouse to the bottom left dot"
+        flags++
+      } else {
+        document.getElementById("instruction").innerHTML = "Failed. Move your mouse to the top left dot"
+        clear();
+        flags = 0
+      }
+    }
+}
+
+function mouseLeave3() {
+    //document.getElementById("four").style.fill = "blue";
+}
+ const dot4 = document.getElementById("four");
+  dot4.addEventListener("mouseenter", mouseEnter4);
+  dot4.addEventListener("mouseleave", mouseLeave4);
+  console.log("enter");
+
+
+function mouseEnter4() {
+ 
+    if (!success) {
+            if (flags === 3) {
+
+      document.getElementById("four").style.fill = "blue";
+    document.getElementById("one").style.fill = "red";
+        document.getElementById("instruction").innerHTML = "Now move your mouse to the top left dot"
+        flags++
+      } else {
+        document.getElementById("instruction").innerHTML = "Failed. Move your mouse to the top left dot"
+        clear();
+        flags = 0
+      }
+    }
+}
+
+function mouseLeave4() {
+   // document.getElementById("one").style.fill = "blue";
+}
+
+function clear() {
+      document.getElementById("one").style.fill = "blue";
+       document.getElementById("two").style.fill = "blue";
+       document.getElementById("three").style.fill = "blue";
+       document.getElementById("four").style.fill = "blue";
+
+}
